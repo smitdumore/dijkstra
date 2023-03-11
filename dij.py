706fb77170baa799a63814e91782a3007473645f
smitdumore <smit.dumore17@vit.edu>
2023-03-11 16:12:36 -0500

    fixed start and goal point issue

--- dij.py
+++ dij.py
@@ -5,58 +5,48 @@ import pygame
 import numpy as np
 sys.setrecursionlimit(100000)
 
-#rect_x, rect_y, rect_width, rect_height = 50, 50, 25, 50
 clearance = 5
 k=2
 tab_height = 250
+tab_width = 600
 
 
-def obstacle_space(x,y, r = 1):
+def obstacle_space(x,y):
 
     c = 0
 
+    # square
     y_c = 50
-    x_c = 50
-    s = 5
+    x_c = 150
+    s = 50
 
-    y1_c = 100
-    x1_c = 100
-    s1 = 10
+    # circle
+    x_circ = 30
+    y_circ = 30
+    r_circ = 10
     
-    x2_c, y2_c = 70, 70  # coordinates of the center of the rhombus
-    s2 = 5            # side length of the rhombus
+    # rhombus
+    x2_c, y2_c = 470, 70 
+    s2 = 10
 
-    # Check if the point is inside the rhombus
-    if abs(x - x2_c) / s2 + abs(y - y2_c) / s2 <= 1:
+    # rhombus
+    #if abs(x - x2_c) / s2 + abs(y - y2_c) / s2 <= 1:
+        #c=1
+    
+    # circle
+    if ((x-math.ceil(x_circ))**2+math.ceil(y -(y_circ))**2-math.ceil(r_circ)**2)<=0:
         c=1
     
-    if ((x-math.ceil(30/r))**2+math.ceil(y -(30/r))**2-math.ceil(5/r)**2)<=0:
-        c=1
-    
+    # square
     if(y <= y_c + s/2 and y >= y_c - s/2 and x <= x_c + s/2 and x >= x_c - s/2):
         c = 1
 
-    if(y <= y1_c + s1/2 and y >= y1_c - s1/2 and x <= x1_c + s1/2 and x >= x1_c - s1/2):
-        c = 1
-
-    return c
-
-
-
-
-
-    # if (38*x- 7*y - 5830/r >= 0) and (38*x + 23*y - 8530/r <= 0) and (37*x -20*y -6101/r <= 0) and (37*x +10*y - 6551/r >= 0):
-    #     c=1
-    # if (x-math.floor(50/r) >= 0) and (x - math.floor(100/r) <= 0) and (y - math.floor(67.5/r) >= 0) and (y - math.floor(112.5/r) <= 0):
-    #     c=1
-    # if ((x-math.ceil(140/r))/math.ceil(15/r))**2 + ((y - math.ceil(120/r))/math.ceil(6/r))**2 - 1 <=0:
-    #     c=1
     return c
 
 def obs():
     obs_space = []
-    for i in range(0,600):
-        for j in range(0,250):
+    for i in range(0,tab_height):
+        for j in range(0,tab_width):
             q = obstacle_space(i,j)
             if q == 1:
                 obs_space.append(tuple((i,j)))
@@ -72,7 +62,6 @@ class Dijkstra:
         self.heap = []
         self.came_from = {}
         self.cost_so_far = {}
-        #self.visited = set()
         self.visited = []
         
     def search(self):
@@ -135,7 +124,10 @@ class Dijkstra:
         
         valid_sucessors = []
         for n in sucessors:
-            if obstacle_space(n[0][0], n[0][1]) == 0 and 0 <= n[0][0] < 600 and 0 <= n[0][1] < 250:
+            if obstacle_space(n[0][0], n[0][1]) == 0 and\
+                    0 <= n[0][0] and n[0][0] < tab_width and\
+                            0 <= n[0][1] and n[0][1] < tab_height:
+                
                 valid_sucessors.append(n)
 
         return valid_sucessors
@@ -210,10 +202,10 @@ class Dijkstra:
 
         #print("size --------------------------", len(occupied))
         #Printing the obstacles
-        my_list = np.array(visited)
-        visited = my_list*k
-        my_list1 = np.array(path)
-        path = my_list1*k
+       # my_list = np.array(visited)
+       # visited = my_list*k
+       # my_list1 = np.array(path)
+       # path = my_list1*k
 
         #my_list2 = np.array(occupied)
         #occupied = my_list2*k
@@ -255,44 +247,38 @@ class Dijkstra:
 
 
 
-occupied = obs()
-#print(occupied)
+
+
 
 def main():
+
+    #generate obstacles
+    occupied = obs()
 
     # get start pos from user
     start_x = int(input("Enter start x coordinate: "))
     start_y = int(input("Enter start y coordinate: "))
     start = (start_x, start_y)
     
-
-    valid_start = False
-
-    while not valid_start:
-        if obstacle_space(start[0], start[1]) == 0 and 0 <= start[0] < 600 and 0 <= start[1] < 250:
-            valid_start = True
-        else:
-            print("Invalid start position. Please enter a valid position.")
-            start_x = int(input("Enter start x coordinate: "))
-            start_y = int(input("Enter start y coordinate: "))
-            start = (start_x, start_y)
-
+    if obstacle_space(start[0], start[1]) == 1:
+        print("start position occupied. Please enter a valid position and run code again.")
+        return
+    elif 0 < start[0] and start[0] >= 600 and 0 < start[1] and start[1] >= 250:
+        print("start position out of bounds. Please enter a valid position and run code again.")
+        return
+        
     # get goal pos from user
     goal_x = int(input("Enter goal x coordinate: "))
     goal_y = int(input("Enter goal y coordinate: "))
     goal = (goal_x, goal_y)
 
-    valid_goal = False
-
-    while not valid_goal:
-        if obstacle_space(goal[0], goal[1]) == 0 and 0 <= goal[0] < 600 and 0 <= goal[1] < 250:
-            valid_goal = True
-        else:
-            print("Invalid goal position. Please enter a valid position.")
-            goal_x = int(input("Enter goal x coordinate: "))
-            goal_y = int(input("Enter goal y coordinate: "))
-            goal = (goal_x, goal_y)
-
+    if obstacle_space(goal[0], goal[1]) == 1:
+        print("goal position occupied. Please enter a valid position and run code again.")
+        return
+    elif 0 < goal[0] and goal[0] >= 600 and 0 < goal[1] and goal[1] >= 250:
+        print("goal position out of bounds. Please enter a valid position and run code again.")
+            
+            
     ############
     # Dijkstra #
     ############
